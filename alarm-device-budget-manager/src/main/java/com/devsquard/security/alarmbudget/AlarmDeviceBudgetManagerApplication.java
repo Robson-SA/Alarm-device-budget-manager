bcc76308588915cfc47f8f630b72fc21c5c8cef2
Robson-SA <robsonk12@hotmail.com>
2025-04-04 23:23:58 -0300

    Add Project

---
+++ alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/AlarmDeviceBudgetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.devsquard.security.alarmbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmDeviceBudgetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmDeviceBudgetManagerApplication.class, args);
+	}
+
+}
---
+++ alarm-device-budget-manager/src/test/java/com/devsquard/security/alarmbudget/AlarmDeviceBudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsquard.security.alarmbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmDeviceBudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
