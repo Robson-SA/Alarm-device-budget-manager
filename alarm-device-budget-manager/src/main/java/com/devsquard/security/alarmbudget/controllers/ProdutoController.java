a8004473b671f7678675565f3d61a74ce0dbb207
curso_básico_Dio <marrimacelso@gmail.com>
2025-04-05 01:35:45 -0300

    + atributos

--- alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/controllers/ProdutoController.java
+++ alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/controllers/ProdutoController.java
@@ -11,5 +11,5 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoService produtoService;
 	
-	p
+	
 }
--- alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/entities/Cliente.java
+++ alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/entities/Cliente.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 
 @Entity
-@Table(name = "tb_client")
+@Table(name = "tb_cliente")
 public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +24,55 @@ public class Cliente {
 	
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Produto> produtos;
+
+	public Cliente(Long id, int contato, int telefone, @Email String email, List<Produto> produtos) {
+		this.id = id;
+		this.contato = contato;
+		this.telefone = telefone;
+		this.email = email;
+		this.produtos = produtos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getContato() {
+		return contato;
+	}
+
+	public void setContato(int contato) {
+		this.contato = contato;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+    
+    
 
 }
