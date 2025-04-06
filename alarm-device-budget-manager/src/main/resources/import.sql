4c98231d85f08da4d5b0f04138a16748041b0d8b
Robson-SA <robsonk12@hotmail.com>
2025-04-06 20:27:56 -0300

    Minha copia do proejeto

--- alarm-device-budget-manager/src/main/resources/import.sql
+++ alarm-device-budget-manager/src/main/resources/import.sql
@@ -1,44 +1,12 @@
-<<<<<<< HEAD
+
 
 ---INSERINDO DADOS NA TABELA TB_PRODUTO---
 
-=======
-<<<<<<< HEAD
 INSERT INTO tb_cliente (nome, cnpj, contato, telefone, email) VALUES ('Alpha Tech Solutions', '12.345.678/0001-10', 'Ana Costa', '(11) 98888-0001', 'ana@alphatech.com');
 INSERT INTO tb_cliente (nome, cnpj, contato, telefone, email) VALUES ('Beta Engenharia', '23.456.789/0001-20', 'Carlos Lima', '(21) 97777-0002', 'carlos@betaeng.com');
 INSERT INTO tb_cliente (nome, cnpj, contato, telefone, email) VALUES ('Gamma Construções', '34.567.890/0001-30', 'Marina Souza', '(31) 96666-0003', 'marina@gammaconst.com');
 INSERT INTO tb_cliente (nome, cnpj, contato, telefone, email) VALUES ('Delta Projetos', '45.678.901/0001-40', 'Paulo Mendes', '(41) 95555-0004', 'paulo@deltaproj.com');
 INSERT INTO tb_cliente (nome, cnpj, contato, telefone, email) VALUES ('Epsilon Instalações', '56.789.012/0001-50', 'Fernanda Rocha', '(51) 94444-0005', 'fernanda@epsilon.com');
-
-INSERT INTO TB_PROJETO (PRJ_DATA, QUANTIDADE, CLIENTE_ID, PRJ_NUMERO, ENDERECO, OBSERVACAO, PRJ_AREA, PRJ_NOME) VALUES ('2025-04-15', 3, 1, 1, 'Rua Alfa, 123', 'Instalação de câmeras', 'Residencial', 'Projeto Segurança Alfa');
-INSERT INTO TB_PROJETO (PRJ_DATA, QUANTIDADE, CLIENTE_ID, PRJ_NUMERO, ENDERECO, OBSERVACAO, PRJ_AREA, PRJ_NOME) VALUES ('2025-04-20', 7, 2, 2, 'Av. Beta, 456', 'Manutenção de alarmes', 'Comercial', 'Projeto Alarme Beta');
-INSERT INTO TB_PROJETO (PRJ_DATA, QUANTIDADE, CLIENTE_ID, PRJ_NUMERO, ENDERECO, OBSERVACAO, PRJ_AREA, PRJ_NOME) VALUES ('2025-04-25', 1, 3, 3, 'Travessa Gama, 789', 'Consultoria de segurança', 'Industrial', 'Projeto Consultoria Gama');
-INSERT INTO TB_PROJETO (PRJ_DATA, QUANTIDADE, CLIENTE_ID, PRJ_NUMERO, ENDERECO, OBSERVACAO, PRJ_AREA, PRJ_NOME) VALUES ('2025-04-30', 12, 1, 4, 'Alameda Delta, 101', 'Expansão do sistema de segurança', 'Residencial', 'Projeto Expansão Delta');
-INSERT INTO TB_PROJETO (PRJ_DATA, QUANTIDADE, CLIENTE_ID, PRJ_NUMERO, ENDERECO, OBSERVACAO, PRJ_AREA, PRJ_NOME) VALUES ('2025-05-05', 5, 4, 5, 'Praça Épsilon, 222', 'Atualização de software de monitoramento', 'Comercial', 'Projeto Update Épsilon');
-=======
->>>>>>> branch 'master' of https://github.com/Robson-SA/Alarm-device-budget-manager.git
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21001', 'Repetidor Audio e Visual 1');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21002', 'Repetidor Audio e Visual 2');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21003', 'Repetidor Audio e Visual 3');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21004', 'Repetidor Audio e Visual 4');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21005', 'Repetidor Audio e Visual 5');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21006', 'Repetidor Audio e Visual 6');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21007', 'Repetidor Audio e Visual 7');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21008', 'Repetidor Audio e Visual 8');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21009', 'Repetidor Audio e Visual 9');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21010', 'Repetidor Audio e Visual 10');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21011', 'Repetidor Audio e Visual 11');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21012', 'Repetidor Audio e Visual 12');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21013', 'Repetidor Audio e Visual 13');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21014', 'Repetidor Audio e Visual 14');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21015', 'Repetidor Audio e Visual 15');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21016', 'Repetidor Audio e Visual 16');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21017', 'Repetidor Audio e Visual 17');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21018', 'Repetidor Audio e Visual 18');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21019', 'Repetidor Audio e Visual 19');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21020', 'Repetidor Audio e Visual 20');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21021', 'Repetidor Audio e Visual 21');
-INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21022', 'Repetidor Audio e Visual 22');
 
 ---INSERINDO DADOS NA TABELA TB-CLIENTE---
 
