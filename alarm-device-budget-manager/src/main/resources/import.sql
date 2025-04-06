dcd4e450e39e39d608c905ababdec8ba2d19bca1
Robson-SA <robsonk12@hotmail.com>
2025-04-06 19:20:35 -0300

    Importing products via SQL

---
+++ alarm-device-budget-manager/src/main/resources/import.sql
@@ -0,0 +1,24 @@
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21001', 'Repetidor Audio e Visual 1');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21002', 'Repetidor Audio e Visual 2');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21003', 'Repetidor Audio e Visual 3');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21004', 'Repetidor Audio e Visual 4');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21005', 'Repetidor Audio e Visual 5');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21006', 'Repetidor Audio e Visual 6');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21007', 'Repetidor Audio e Visual 7');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21008', 'Repetidor Audio e Visual 8');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21009', 'Repetidor Audio e Visual 9');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21010', 'Repetidor Audio e Visual 10');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21011', 'Repetidor Audio e Visual 11');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21012', 'Repetidor Audio e Visual 12');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21013', 'Repetidor Audio e Visual 13');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21014', 'Repetidor Audio e Visual 14');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21015', 'Repetidor Audio e Visual 15');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21016', 'Repetidor Audio e Visual 16');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21017', 'Repetidor Audio e Visual 17');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21018', 'Repetidor Audio e Visual 18');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21019', 'Repetidor Audio e Visual 19');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21020', 'Repetidor Audio e Visual 20');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21021', 'Repetidor Audio e Visual 21');
+INSERT INTO tb_produto (PRO_COD, PRO_NOME) VALUES ('21022', 'Repetidor Audio e Visual 22');
+
+
