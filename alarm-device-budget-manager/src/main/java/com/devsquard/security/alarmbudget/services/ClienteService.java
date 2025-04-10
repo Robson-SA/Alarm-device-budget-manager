bf0330e4f944526013505fca7115f66dee930f9b
Robson-SA <robsonk12@hotmail.com>
2025-04-09 21:28:41 -0300

    Added clientService

--- alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/services/ClienteService.java
+++ alarm-device-budget-manager/src/main/java/com/devsquard/security/alarmbudget/services/ClienteService.java
@@ -7,16 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.devsquard.security.alarmbudget.dto.ClienteDTO;
 import com.devsquard.security.alarmbudget.entities.Cliente;
 import com.devsquard.security.alarmbudget.entities.Projeto;
 import com.devsquard.security.alarmbudget.repositories.ClienteRepository;
-import com.devsquard.security.alarmbudget.services.exceptions.ResourceNotFoundException;
+
+import jakarta.transaction.Transactional;
 
 @Service
-public class ClienteService {
+public class ClienteService{
 
 	@Autowired
 	private ClienteRepository clienteRepository;
@@ -24,51 +24,64 @@ public class ClienteService {
 	@Transactional
 	public Cliente save(ClienteDTO dto) {
 		Cliente cliente = new Cliente();
-
+		
 		cliente.setNome(dto.getNome());
 	    cliente.setCnpj(dto.getCnpj());
 	    cliente.setContato(dto.getContato());
 	    cliente.setTelefone(dto.getTelefone());
 	    cliente.setEmail(dto.getEmail());
+	      
+	    if (dto.getProjetos() != null) {
+	        Set<Projeto> projetos = dto.getProjetos().stream().map(projDTO -> {
+	            Projeto p = new Projeto();
+	            p.setCodigo(projDTO.getCodigo());
+	            p.setNome(projDTO.getNome());
+	            p.setArea(projDTO.getArea());
+	            p.setEndereco(projDTO.getEndereco());
+	            p.setQuantidade(projDTO.getQuantidade());
+	            p.setObservacao(projDTO.getObservacao());
+	            p.setData(projDTO.getData());
+	            p.setCliente(cliente);
+	            return p;
+	        }).collect(Collectors.toSet());
 
-	    Set<Projeto> projetos = dto.getProjetos().stream().map(projDTO -> {
-	        Projeto p = new Projeto();
-	        p.setCodigo(projDTO.getCodigo());
-	        p.setNome(projDTO.getNome());
-	        p.setArea(projDTO.getArea());
-	        p.setEndereco(projDTO.getEndereco());
-	        p.setQuantidade(projDTO.getQuantidade());
-	        p.setObservacao(projDTO.getObservacao());
-	        p.setData(projDTO.getData());
-	        p.setCliente(cliente);
-	        return p;
-	    }).collect(Collectors.toSet());
+	        cliente.setProjetos(projetos);
+	    }
 
-	    cliente.setProjetos(projetos);
+	    if (dto.getProjetos() != null) {
+	        Set<Projeto> projetos = dto.getProjetos().stream().map(projDTO -> {
+	            Projeto p = new Projeto();
+	            p.setCodigo(projDTO.getCodigo());
+	            p.setNome(projDTO.getNome());
+	            p.setArea(projDTO.getArea());
+	            p.setEndereco(projDTO.getEndereco());
+	            p.setQuantidade(projDTO.getQuantidade());
+	            p.setObservacao(projDTO.getObservacao());
+	            p.setData(projDTO.getData());
+	            p.setCliente(cliente);
+	            return p;
+	        }).collect(Collectors.toSet());
 
-		return clienteRepository.save(cliente);
+	        cliente.setProjetos(projetos);
+	    }
 
+	    return clienteRepository.save(cliente);
+		
 	}
-
+	
 	@Transactional
 	public Page<ClienteDTO> findAll(Pageable pageable) {
-		return clienteRepository.findAll(pageable)
-				.map(ClienteDTO::new);
+	    return clienteRepository.findAll(pageable)
+	            .map(ClienteDTO::new);
 	}
-
+	
 	@Transactional
 	public Page<ClienteDTO> findByNome(String nome, Pageable pageable) {
-		Page<Cliente> clienteExiste = clienteRepository.findByNome(nome, pageable);
-
-		return clienteExiste.map(x -> new ClienteDTO(x));
-
+		 Page<Cliente> clienteExiste = clienteRepository.findByNome(nome,pageable);
+				
+		 return clienteExiste.map(x -> new ClienteDTO(x));
+		 
+		
 	}
-	@Transactional(readOnly = true)
-	public ClienteDTO findById(Long id) {
-		Cliente cliente = clienteRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new ClienteDTO(cliente);
-	}
-	}
-
-
+	
+}
